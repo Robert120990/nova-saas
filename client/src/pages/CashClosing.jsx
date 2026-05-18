87888111ab9460213c5caa5b4e397b7534e38a1c
Roberto <jr_1209@outlook.com>
2026-05-17 20:29:42 -0600

    fix: async onClick for arqueo print

--- client/src/pages/CashClosing.jsx
+++ client/src/pages/CashClosing.jsx
@@ -622,7 +622,7 @@ const CashClosing = () => {
                                 </div>
 
                                 <button 
-                                    onClick={() => {
+                                onClick={async () => {
                                         const totalCashIncomings = (parseFloat(shiftSummary.opening_balance) || 0) + (parseFloat(shiftSummary.cash) || 0) + incomes.filter(i => i.payment_method === '01').reduce((acc, e) => acc + (parseFloat(e.amount) || 0), 0);
                                         const totalExp = expenses.reduce((acc, e) => acc + (parseFloat(e.amount) || 0), 0);
                                         if (totalExp > totalCashIncomings) {
