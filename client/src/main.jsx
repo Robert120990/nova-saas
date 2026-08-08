e97d60138fa72767bac226024e6da4e3bcc81aba
Roberto <jr_1209@outlook.com>
2026-08-08 08:55:08 -0600

    feat: PWA auto-update completo con recarga automatica y chequeo horario de versiones

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
+setInterval(() => {
+    if ('serviceWorker' in navigator)
+        navigator.serviceWorker.getRegistration().then(reg => reg?.update())
+}, 60 * 60 * 1000)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
