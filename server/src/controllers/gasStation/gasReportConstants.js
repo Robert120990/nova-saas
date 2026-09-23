9e98e2323a484f14c2f7ea94cdca0af77e939150
Roberto <jr_1209@outlook.com>
2026-09-23 17:39:14 -0600

    refactor(backend): modularizar controladores contables y de combustible y actualizar nodemailer

---
+++ server/src/controllers/gasStation/gasReportConstants.js
@@ -0,0 +1,28 @@
+/**
+ * Gas Station Report Constants
+ */
+
+const tipoNombres = {
+    remesas: 'Remesas',
+    gastos: 'Gastos',
+    creditos: 'Créditos',
+    cupones: 'Cupones',
+    descuentos: 'Descuentos',
+    adelantos: 'Adelantos',
+    tarjetas: 'Tarjetas',
+    vales: 'Vales',
+    anticipos_desp: 'Anticipos Despachados',
+    lubricantes: 'Lubricantes'
+};
+
+const fuelTypeLabels = {
+    1: 'REGULAR',
+    2: 'SUPER',
+    3: 'DIESEL',
+    4: 'ION DIESEL'
+};
+
+module.exports = {
+    tipoNombres,
+    fuelTypeLabels
+};
